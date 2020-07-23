9e41b759c04f68579baf0f1d363f1451fe7e97c6
DNanditha <68146070+DNanditha@users.noreply.github.com>
2020-07-23 12:28:29 +0530

    Add files via upload

---
+++ D3Q3.js
@@ -0,0 +1,25 @@
+var marks=prompt("Enter your marks");
+if(marks<=60)
+{
+    console.log("Marks is "+marks+" and grade is E");
+}
+else if(marks<=70)
+{
+    console.log("Marks is "+marks+" and grade is D");
+}
+else if(marks<=80)
+{
+    console.log("Marks is "+marks+" and grade is C");
+}
+else if(marks<=90)
+{
+    console.log("Marks is "+marks+" and grade is B");
+}
+else if(marks<=100)
+{
+    console.log("Marks is "+marks+" and grade is A");
+}
+else
+{
+    console.log("Enter the valid marks");
+}
